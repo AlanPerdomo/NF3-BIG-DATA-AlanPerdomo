d9d45a203d65267970156c803ac295495a29299c
Alan <alanperdomo@hotmail.com>
2023-06-12 11:24:33 -0300

    .

---
+++ 2/2.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+url = 'https://raw.githubusercontent.com/AlanPerdomo/NF3-BIG-DATA-AlanPerdomo/blob/main/2/cursos-prouni.csv'
+data = pd.read_csv(url)
+
+print(data)
+
+#https://github.com/AlanPerdomo/NF3-BIG-DATA-AlanPerdomo/blob/main/2/cursos-prouni.csv.xlsx
