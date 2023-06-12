64c0c063547f2937669ac56d30b3fa15037c0688
Alan <alanperdomo@hotmail.com>
2023-06-12 11:46:15 -0300

    .

--- 2/2.py
+++ 2/2.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-url = 'https://raw.githubusercontent.com/AlanPerdomo/NF3-BIG-DATA-AlanPerdomo/main/cursos-prouni.csv'
+url = 'https://raw.githubusercontent.com/AlanPerdomo/NF3-BIG-DATA-AlanPerdomo/main/2/cursos-prouni.csv'
 data = pd.read_csv(url)
 
 
