4c30895994beca573e0f368e0bccc5b9712b7d74
Alan <alanperdomo@hotmail.com>
2023-06-16 12:34:35 -0300

    .

--- 1/1.py
+++ 1/1.py
@@ -1,21 +1,52 @@
 import pandas as pd
 
-data = pd.read_csv("1\world_alcohol.csv")
+url = "https://raw.githubusercontent.com/AlanPerdomo/NF3-BIG-DATA-AlanPerdomo/main/1/world_alcohol.csv"
+data = pd.read_csv(url,encoding='latin-1')
 
-# A)
-tipoBebidas = data.groupby("Beverage Types")["Beverage Types"].count()
-# B)
+'''
+# A) ----------------------------
+print("Questao A:")
+tipoBebidas = data.groupby("Beverage Types")
+for tipo, grupo in tipoBebidas:
+    print("Tipo de bebida:",tipo)
+    print(grupo)
+    print()
+
+# B) ----------------------------
+print("Questao B:")
 anoRegiao = data.groupby(["WHO region", "Year"])
-# C)
+for (regiao, ano), grupo in anoRegiao:
+    print("Região:", regiao)
+    print("Ano:", ano)
+    print(grupo)
+    print()
+'''
+# C) ----------------------------
+print("Questao C:")
+
 contagemRegioes = data["WHO region"].value_counts()
+print("Contagem de Regiões:")
+print(contagemRegioes)
+print()
+
+
+
 contagemPaises = data["Country"].value_counts()
 somaValoresPorBebidas = data.groupby("Beverage Types")["Display Value"].sum()
-# D)
+# D) ----------------------------
+print("Questao D:")
 statusValores = data.groupby("Beverage Types")["Display Value"].describe()
-# E.1)
+# E.1) ----------------------------
+print("Questao E.I:")
 bebidas1985 = data[data["Year"] == 1985]["Beverage Types"]
-# E.2)
+# E.2) ----------------------------
+print("Questao E.II:")
 regiao = data[data["Display Value"] > 4]["WHO region"]
 
 
-print("Dados agrupados por tipo de bebidas: \n", tipoBebidas)
+
+
+
+
+
+
